cbcc8112aed4a7cbb9a2fe38b8a82ea048db5cab
vladutflaviusp <vladutflaviusp@gmail.com>
2026-08-28 23:37:30 +0300

    feat: add TCP protocol structures

---
+++ include/protocol.hpp
@@ -0,0 +1,37 @@
+#ifndef PROTOCOL_HPP
+#define PROTOCOL_HPP
+
+#include <cstdint>
+
+const int SERVER_PORT = 8080;
+const int MAX_LOCKERS = 10;
+
+enum class MessageType : uint8_t {
+    CLIENT_LOGIN = 1,     
+    DEPOSIT_PACKAGE = 2,  
+    PICKUP_PACKAGE = 3,   
+    SERVER_RESPONSE = 4  
+};
+
+enum class StatusCode : uint8_t {
+    SUCCESS = 200,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    NOT_FOUND = 404,
+    BOX_FULL = 507
+};
+
+struct MessageHeader {
+    MessageType type;
+    uint8_t lockerId;    
+    StatusCode status;   
+    uint16_t dataLength; 
+};
+
+struct PackageData {
+    char pin[5];         
+    bool isOccupied;    
+};
+
+
+#endif
