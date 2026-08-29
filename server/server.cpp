4301161efcd686e7caee941e71812c64304013b9
vladutflaviusp <vladutflaviusp@gmail.com>
2026-08-29 13:37:32 +0300

    feat: implement initial TCP server setup

---
+++ server/server.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "../include/protocol.hpp"
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+
+    WSADATA wsaData;
+    int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaResult != 0) {
+        std::cerr << "WSAStartup failed: " << wsaResult << "\n";
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed: " << WSAGetLastError() << "\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(SERVER_PORT);
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Listen failed: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Parcel Locker Network - Server is running and listening on port " << SERVER_PORT << "...\n";
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
