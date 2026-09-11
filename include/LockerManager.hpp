6672d63e368c16ce53a05ba8e59d0f8e1bd783e4
vladutflaviusp <vladutflaviusp@gmail.com>
2026-09-11 16:49:39 +0300

    feat: integrate sqlite persistence into locker manager

--- include/LockerManager.hpp
+++ include/LockerManager.hpp
@@ -1,52 +1,85 @@
 #ifndef LOCKER_MANAGER_HPP
 #define LOCKER_MANAGER_HPP
 
-#include <vector>
-#include <cstring>
-#include "protocol.hpp"
-
-struct LockerServerState {
-    bool isOccupied = false;
-    char pin[5] = {0};
-};
+#include "Database.hpp"
+#include <string>
 
 class LockerManager {
 private:
-    std::vector<LockerServerState> lockers;
+    Database& db;
 
 public:
-    LockerManager() : lockers(MAX_LOCKERS) {}
+    explicit LockerManager(Database& database) : db(database) {}
 
-    bool isOccupied(uint8_t lockerId) const {
-        if (lockerId >= MAX_LOCKERS) return false;
-        return lockers[lockerId].isOccupied;
+    bool isOccupied(int lockerId) {
+        std::string query = "SELECT is_occupied FROM lockers WHERE id = " + std::to_string(lockerId) + ";";
+        sqlite3_stmt* stmt;
+        bool occupied = false;
+
+        if (sqlite3_prepare_v2(db.getHandle(), query.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+            if (sqlite3_step(stmt) == SQLITE_ROW) {
+                occupied = sqlite3_column_int(stmt, 0) != 0;
+            }
+            sqlite3_finalize(stmt);
+        }
+        return occupied;
     }
 
-    StatusCode deposit(uint8_t lockerId, const char* pin) {
-        if (lockerId >= MAX_LOCKERS) {
-            return StatusCode::NOT_FOUND;
+    bool occupyLocker(int lockerId, const std::string& pin) {
+        std::string query = "INSERT OR REPLACE INTO lockers (id, is_occupied, pin) VALUES (?, 1, ?);";
+        sqlite3_stmt* stmt;
+        bool success = false;
+
+        if (sqlite3_prepare_v2(db.getHandle(), query.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+            sqlite3_bind_int(stmt, 1, lockerId);
+            sqlite3_bind_text(stmt, 2, pin.c_str(), -1, SQLITE_STATIC);
+
+            if (sqlite3_step(stmt) == SQLITE_DONE) {
+                success = true;
+            }
+            sqlite3_finalize(stmt);
         }
-        if (lockers[lockerId].isOccupied) {
-            return StatusCode::BOX_FULL;
-        }
-        lockers[lockerId].isOccupied = true;
-        std::memcpy(lockers[lockerId].pin, pin, 5);
-        return StatusCode::SUCCESS;
+        return success;
     }
 
-    StatusCode pickup(uint8_t lockerId, const char* pin) {
-        if (lockerId >= MAX_LOCKERS) {
-            return StatusCode::NOT_FOUND;
+    bool releaseLocker(int lockerId, const std::string& pin) {
+        if (!verifyPin(lockerId, pin)) {
+            return false;
         }
-        if (!lockers[lockerId].isOccupied) {
-            return StatusCode::NOT_FOUND;
+
+        std::string query = "UPDATE lockers SET is_occupied = 0, pin = '' WHERE id = ?;";
+        sqlite3_stmt* stmt;
+        bool success = false;
+
+        if (sqlite3_prepare_v2(db.getHandle(), query.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+            sqlite3_bind_int(stmt, 1, lockerId);
+
+            if (sqlite3_step(stmt) == SQLITE_DONE) {
+                success = true;
+            }
+            sqlite3_finalize(stmt);
         }
-        if (std::memcmp(lockers[lockerId].pin, pin, 5) != 0) {
-            return StatusCode::UNAUTHORIZED;
+        return success;
+    }
+
+private:
+    bool verifyPin(int lockerId, const std::string& pin) {
+        std::string query = "SELECT pin FROM lockers WHERE id = ? AND is_occupied = 1;";
+        sqlite3_stmt* stmt;
+        bool valid = false;
+
+        if (sqlite3_prepare_v2(db.getHandle(), query.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+            sqlite3_bind_int(stmt, 1, lockerId);
+
+            if (sqlite3_step(stmt) == SQLITE_ROW) {
+                const unsigned char* storedPin = sqlite3_column_text(stmt, 0);
+                if (storedPin && pin == reinterpret_cast<const char*>(storedPin)) {
+                    valid = true;
+                }
+            }
+            sqlite3_finalize(stmt);
         }
-        lockers[lockerId].isOccupied = false;
-        std::memset(lockers[lockerId].pin, 0, 5);
-        return StatusCode::SUCCESS;
+        return valid;
     }
 };
 
